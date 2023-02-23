fe4a58ac079665d93e36c9dea4330ae39a94f03e
nzht <124674475+0x-nzht@users.noreply.github.com>
2023-02-23 21:36:02 +0900

    Update socials.ts

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -11,7 +11,7 @@ export const socials = [
   },
   {
     name: 'icon',
-    url: '../../../src/pages/except/profile.paths.md',
+    url: '../../../src/pages/blog/web-design-trends-for-2023',
     icon: 'mdi:account'
   }
 ]
