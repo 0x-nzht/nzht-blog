ec173e45f39dcdfb599669b7f380ff151eae704d
nzht <124674475+0x-nzht@users.noreply.github.com>
2023-02-23 17:25:51 +0900

    Update socials.ts

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -8,10 +8,5 @@ export const socials = [
     name: 'GitHub',
     url: 'https://github.com/littlesticks',
     icon: 'mdi:github'
-  },
-  {
-    name: 'Instagram',
-    url: 'https://instagram.com',
-    icon: 'mdi:instagram'
   }
 ]
